4adf0f06a6745f030945a61b8fd91be5b587b162
fwara07 <fanuelwara05@gmail.com>
2021-03-26 23:27:13 -0400

    fixed bugs

--- src/components/containers/SpendingPerCategory.js
+++ src/components/containers/SpendingPerCategory.js
@@ -24,6 +24,7 @@ const SpendingPerCategory = ({ className, ...rest }) => {
   const classes = useStyles();
   const theme = useTheme();
   const [series, setSeries] = useState([]);
+  const [error, setError] = useState(false);
 
   const colorsLst = [
     colors.yellow[700],
@@ -42,7 +43,11 @@ const SpendingPerCategory = ({ className, ...rest }) => {
     })
       .then((res) => res.json())
       .then((json) => {
-        setSeries(Object.values(json));
+        if (!json.hasOwnProperty("msg")) {
+          setSeries(Object.values(json));
+        } else {
+          setError(true);
+        }
       });
   }, []);
 
@@ -101,23 +106,61 @@ const SpendingPerCategory = ({ className, ...rest }) => {
     <Card className={clsx(classes.root, className)} {...rest}>
       <CardHeader title="Spending Per Category" />
       <Divider />
-      <CardContent>
-        <Box height={300} position="relative">
-          <Doughnut data={data} options={options} />
-        </Box>
-        <Box display="flex" justifyContent="center" mt={2}>
-          {getCategoryChartData().map(({ textColor, title, value }) => (
-            <Box key={title} p={0.5} textAlign="center">
-              <Typography color="textPrimary" variant="body1">
-                {title}
-              </Typography>
-              <Typography style={{ color: textColor }} variant="h4">
-                {value}%
-              </Typography>
-            </Box>
-          ))}
-        </Box>
-      </CardContent>
+      {this.error ? (
+        <CardContent>
+          <Box height={300} position="relative">
+            <svg width="100%" height="100%" viewBox="0 0 42 42" class="donut">
+              <circle
+                class="donut-hole"
+                cx="21"
+                cy="21"
+                r="15.91549430918954"
+                fill="#fff"
+              ></circle>
+              <circle
+                class="donut-ring"
+                cx="21"
+                cy="21"
+                r="15.91549430918954"
+                fill="transparent"
+                stroke="#d2d3d4"
+                stroke-width="3"
+              ></circle>
+
+              <circle
+                class="donut-segment"
+                cx="21"
+                cy="21"
+                r="15.91549430918954"
+                fill="transparent"
+                stroke="#ce4b99"
+                stroke-width="3"
+              ></circle>
+            </svg>
+            <Typography variant="h4" gutterBottom>
+              You have not spent yet.
+            </Typography>
+          </Box>
+        </CardContent>
+      ) : (
+        <CardContent>
+          <Box height={300} position="relative">
+            <Doughnut data={data} options={options} />
+          </Box>
+          <Box display="flex" justifyContent="center" mt={2}>
+            {getCategoryChartData().map(({ textColor, title, value }) => (
+              <Box key={title} p={0.5} textAlign="center">
+                <Typography color="textPrimary" variant="body1">
+                  {title}
+                </Typography>
+                <Typography style={{ color: textColor }} variant="h4">
+                  {value}%
+                </Typography>
+              </Box>
+            ))}
+          </Box>
+        </CardContent>
+      )}
     </Card>
   );
 };
