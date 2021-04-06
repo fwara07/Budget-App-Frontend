afc3fc8e2003feb9bcfd81d76ae46fe4916d4a8b
fwara07 <fanuelwara05@gmail.com>
2021-04-06 09:45:00 -0400

    fix bugs

--- src/components/actions/actions.js
+++ src/components/actions/actions.js
@@ -13,24 +13,30 @@ export const getAllCategories = async (setCategoriesState) => {
 };
 
 export const getBalance = async (setBalanceState, state) => {
-  let response = await fetch("https://budget-planning.herokuapp.com/api/get-balance", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `JWT ${localStorage.getItem("token")}`,
-    },
-    body: JSON.stringify({ category: state.category }),
-  })
-  let json = await response.json()
+  let response = await fetch(
+    "https://budget-planning.herokuapp.com/api/get-balance",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({ category: state.category }),
+    }
+  );
+  let json = await response.json();
   setBalanceState(json);
 };
 
-export const getTotalHistoryData = (setHistoryData) => {
-  let response = await fetch("https://budget-planning.herokuapp.com/api/total-history", {
-    headers: {
-      Authorization: `JWT ${localStorage.getItem("token")}`,
-    },
-  })
-  let json = await reponse.json()
+export const getTotalHistoryData = async (setHistoryData) => {
+  let response = await fetch(
+    "https://budget-planning.herokuapp.com/api/total-history",
+    {
+      headers: {
+        Authorization: `JWT ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+  let json = await reponse.json();
   setHistoryData(json);
 };
